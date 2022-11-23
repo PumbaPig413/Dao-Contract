a2076d4707397d31e0f37f63ea046159b3391162
PumbaPig413 <PumbaPig413@gmail.com>
2022-11-23 14:30:33 +0900

    First Dao Contract Project is created.

---
+++ packages/cwd-macros/tests/govmod.rs
@@ -0,0 +1,31 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+use cwd_macros::proposal_module_query;
+
+#[proposal_module_query]
+#[allow(dead_code)]
+#[cw_serde]
+#[derive(QueryResponses)]
+enum Test {
+    #[returns(String)]
+    Foo,
+    #[returns(String)]
+    Bar(u64),
+    #[returns(String)]
+    Baz { waldo: u64 },
+}
+
+#[test]
+fn proposal_module_query_derive() {
+    let test = Test::Dao {};
+
+    // If this compiles we have won.
+    match test {
+        Test::Foo | Test::Bar(_) | Test::Baz { .. } | Test::Dao {} => "yay",
+        Test::Info {} => "yay",
+        Test::ProposalCount {} => "yay",
+        Test::ProposalCreationPolicy {} => "yay",
+        Test::ProposalHooks {} => "yay",
+        Test::VoteHooks {} => "yay",
+    };
+}
